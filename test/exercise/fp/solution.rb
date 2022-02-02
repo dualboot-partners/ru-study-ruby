e81f55c5406f809c0477dfddf035948a06624275
mrandell <zamagsvoishi@gmail.com>
2022-02-02 11:00:33 +0300

    Added fp solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,33 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        sum = array.reduce(0) do |acc, film|
+          if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive? && \
+             !film['country'].nil? && film['country'].to_s.split(',').count >= 2
+            acc += film['rating_kinopoisk'].to_f
+          end
+          acc
+        end
+        num = array.reduce(0) do |acc, film|
+          if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive? && \
+             !film['country'].nil? && film['country'].to_s.split(',').count >= 2
+            acc += 1
+          end
+          acc
+        end
+        sum / num
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        array = films.reduce('') do |acc, film|
+          acc << film['name'].to_s if film['rating_kinopoisk'].to_f >= threshold
+          acc
+        end
+        array.split('').reduce(0) do |num, char|
+          num += 1 if char == 'и'
+          num
+        end
       end
     end
   end
--- test/exercise/fp/test.rb
+++ test/exercise/fp/test.rb
@@ -7,7 +7,6 @@ class Exercise::FpTest < Minitest::Test
   # Посчитать средний рейтинг фильмов по версии кинопоиска у которых две или больше стран
   # Фильмы у которых рейтиг не задан или равен 0 не учитывать в расчете среднего.
   def test_rating
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.rating(array)
@@ -18,7 +17,6 @@ class Exercise::FpTest < Minitest::Test
 
   # Посчитать количесвто букв 'и' в названиях всех фильмов с рейтингом кинопоиска больше или равным заданному значению
   def test_chars_count
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.chars_count(array, 5)
