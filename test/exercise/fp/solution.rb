adc8913934d04a8a820d792e98f1b03fd3bed9c0
mrandell <zamagsvoishi@gmail.com>
2022-02-07 23:46:53 +0300

    Fix fp solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,29 +5,22 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        sum = array.reduce(0) do |acc, film|
+        filtered_films = array.map do |film|
           if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive? && \
              !film['country'].nil? && film['country'].to_s.split(',').count >= 2
-            acc += film['rating_kinopoisk'].to_f
+            film
           end
-          acc
         end
-        num = array.reduce(0) do |acc, film|
-          if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive? && \
-             !film['country'].nil? && film['country'].to_s.split(',').count >= 2
-            acc += 1
-          end
-          acc
-        end
-        sum / num
+        rating_array = filtered_films.compact.map { |film| film['rating_kinopoisk'].to_f }
+        rating_array.reduce(:+) / rating_array.size
       end
 
       def chars_count(films, threshold)
-        array = films.reduce('') do |acc, film|
+        names_string = films.reduce('') do |acc, film|
           acc << film['name'].to_s if film['rating_kinopoisk'].to_f >= threshold
           acc
         end
-        array.split('').reduce(0) do |num, char|
+        names_string.chars.reduce(0) do |num, char|
           num += 1 if char == 'и'
           num
         end
