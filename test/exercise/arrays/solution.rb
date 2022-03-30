0454b7664c62eee7104e295e4a24f4f14a9f4ed7
art v <artvpublic@gmail.com>
2022-03-30 14:04:31 +0300

    complete arrays exercises

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,25 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |el| el.positive? ? array.max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        iter = lambda { |first_ind, last_ind|
+          return -1 if first_ind > last_ind
+
+          middle_ind = (first_ind + last_ind) / 2
+          middle_item = array[middle_ind]
+
+          return middle_ind if middle_item == query
+
+          new_first_ind = middle_item < query ? middle_ind + 1 : first_ind
+          new_last_ind = middle_item < query ? last_ind : middle_ind - 1
+
+          iter.call(new_first_ind, new_last_ind)
+        }
+
+        iter.call(0, array.length - 1)
       end
     end
   end
