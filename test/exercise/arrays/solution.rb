dd5443b17112f23f16b0562d57b4e1f7a6150634
vslv <vslv.anpilov@gmail.com>
2022-05-21 01:22:41 +0300

    Solve 'arrays' exercise

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,30 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_num = array.max
+
+        array.each_with_index do |elem, index|
+          array[index] = max_num if elem.positive?
+        end
       end
 
       def search(_array, _query)
-        0
+        min_num = 0
+        avg_num = _array.size / 2
+        max_num = _array.size - 1
+
+        return -1 unless _array.include?(_query)
+
+        while _array[avg_num] != _query && min_num <= max_num
+          if _query > _array[avg_num]
+            min_num = avg_num + 1
+          else
+            max_num = avg_num - 1
+          end
+
+          avg_num = (min_num + max_num) / 2
+        end
+        avg_num
       end
     end
   end
