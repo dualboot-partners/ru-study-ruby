78c97f4c3d3fffdb8cc08048ba18bf60598c2b43
Mihail Penin <peninmg@gmail.com>
2022-05-30 16:20:35 +0300

    Solved fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,58 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        return self unless block_given?
+
+        i = 0
+        while i < size
+          yield self[i]
+          i += 1
+        end
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        return self unless block_given?
+
+        result = MyArray.new
+        i = 0
+        while i < size
+          result << (yield self[i])
+          i += 1
+        end
+        result
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = MyArray.new
+        i = 0
+        while i < size
+          result << self[i] unless self[i].nil?
+          i += 1
+        end
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        return nil unless block_given?
+        return nil if empty?
+
+        i = 0
+        if acc.nil?
+          acc = self[0]
+          i = 1
+        end
+
+        while i < size
+          acc = (yield acc, self[i])
+          i += 1
+        end
+        acc
+      end
     end
   end
 end
