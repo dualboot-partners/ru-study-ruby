a5599814de4b075884c7a9fbe41118ed5ebf752e
Mihail Penin <peninmg@gmail.com>
2022-05-30 15:42:12 +0300

    Solved fp

--- test/exercise/fp/test.rb
+++ test/exercise/fp/test.rb
@@ -7,10 +7,10 @@ class Exercise::FpTest < Minitest::Test
   # Посчитать средний рейтинг фильмов по версии кинопоиска у которых две или больше стран
   # Фильмы у которых рейтиг не задан или равен 0 не учитывать в расчете среднего.
   def test_rating
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.rating(array)
+
     # rubocop:disable Lint/FloatComparison
     assert result == 6.809410385259628
     # rubocop:enable Lint/FloatComparison
@@ -18,7 +18,6 @@ class Exercise::FpTest < Minitest::Test
 
   # Посчитать количесвто букв 'и' в названиях всех фильмов с рейтингом кинопоиска больше или равным заданному значению
   def test_chars_count
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.chars_count(array, 5)
