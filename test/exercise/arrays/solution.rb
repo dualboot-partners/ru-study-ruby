3e73a3e03a258552deac7310448565de018ca1d5
nick <nikolai.roshchin@gmail.com>
2022-07-11 10:32:23 +0300

    Array replace method added

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,20 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = maximum(array)
+        new_array = []
+        array.each do |arr|
+          new_array << (arr.positive? ? max : arr)
+        end
+        new_array
+      end
+
+      def maximum(array)
+        max = array[0]
+        array.each do |arr|
+          max = arr if arr > max
+        end
+        max
       end
 
       def search(_array, _query)
