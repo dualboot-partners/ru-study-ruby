61fe30340719f7fca7b75b97abf0b4df4aed23fe
Spola1 <1111.22224@mail.ru>
2022-09-19 22:27:04 +0300

    arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,21 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map do |el|
+          if el >= 0
+            el = array.max
+          else
+            el
+          end
+        end
       end
 
       def search(_array, _query)
-        0
+        if _array.empty? || _array.include?(_query) == false
+          -1
+        else
+          _array.map.with_index { |a, i| a == _query ? i : nil }.compact.first
+        end
       end
     end
   end
