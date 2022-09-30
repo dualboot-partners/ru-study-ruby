a3276b6eef0ab7051693e56b59a94f16fe2a018a
Yarex Marvin <yarexmarvin@yahoo.com>
2022-09-30 19:45:05 +0400

    completed tasks

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,22 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_countries = array.select do |film|
+          !film['country'].nil? && film['country'].split(',').length > 1 && !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive?
+        end
+        total_kinopoisk_score = filtered_countries.reduce(0) { |sum, film| sum + film['rating_kinopoisk'].to_f }
+        total_kinopoisk_score / filtered_countries.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.reduce(0) do |sum, film|
+          if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold
+            letters = film['name'].chars.count('и')
+            sum += letters
+          end
+          sum
+        end
       end
     end
   end
--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,60 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        count = 0
+        size = self.size
+
+        while count < size
+          yield(self[count])
+          count += 1
+        end
+
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = MyArray.new
+        count = 0
+        size = self.size
+
+        while count < size
+          result << yield(self[count])
+          count += 1
+        end
+
+        result
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = MyArray.new
+        count = 0
+        size = self.size
+
+        while count < size
+          result << self[count] unless self[count].nil?
+          count += 1
+        end
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(initial_value = nil)
+        count = 0
+        size = self.size
+
+        head, *tail = self
+        acc = initial_value.nil? ? head : initial_value.to_i
+        arr = initial_value.nil? ? tail : self
+
+        while count < size
+          acc = yield(acc, arr[count]) unless arr[count].nil?
+          count += 1
+        end
+        acc
+      end
     end
   end
 end
