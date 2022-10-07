127fae083af4afe0cd6c9dfa6e6941c0edfb3ad5
CrsPet <serezha.petrov.02@gmail.com>
2022-10-07 14:17:25 +0300

    impl rating, chars_count functions

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,18 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_films = array.select { |el| el['country'] && el['country'].split(',').length > 1 && el['rating_kinopoisk'] }
+                              .map { |el| el['rating_kinopoisk'].to_f }
+                              .select(&:positive?)
+
+        filtered_films.reduce { |sum, num| sum + num } / filtered_films.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.select { |el| el['rating_kinopoisk'] && el['rating_kinopoisk'].to_f >= threshold }
+             .map { |el| el['name'].count('и') }
+             .reduce(:+)
       end
     end
   end
