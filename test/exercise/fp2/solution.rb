d2f84bb0dfcca6640df4898367dc35e0e4094495
CrsPet <serezha.petrov.02@gmail.com>
2022-10-08 13:49:40 +0300

    impl functional programming primitives

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,36 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for el in self do
+          yield el
+        end
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        my_reduce(MyArray.new) { |arr, el| arr << (yield el) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(MyArray.new) { |arr, el| el.nil? ? arr : arr << el }
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        return acc if length.zero?
+
+        arr = self
+        if acc.nil?
+          acc = self [0]
+          arr = MyArray.new(self [1, length])
+        end
+        arr.my_each { |el| acc = yield(acc, el) }
+
+        acc
+      end
     end
   end
 end
--- test/exercise/fp2/test.rb
+++ test/exercise/fp2/test.rb
@@ -11,7 +11,7 @@ class Exercise::Fp2Test < Minitest::Test
   end
 
   def test_my_each
-    skip
+    # skip
     result = []
     my_result = []
 
@@ -23,14 +23,14 @@ class Exercise::Fp2Test < Minitest::Test
   end
 
   def test_my_map
-    skip
+    # skip
     func = ->(element) { element * @int }
     assert @array.map(&func) == @my_array.my_map(&func)
     assert @array.map(&func).map(&func) == @my_array.my_map(&func).my_map(&func)
   end
 
   def test_my_compact
-    skip
+    # skip
     func = ->(element) { element if element.even? }
     func_another = ->(element) { element * @int }
     func_yet_another = ->(element) { element.even? }
@@ -40,7 +40,7 @@ class Exercise::Fp2Test < Minitest::Test
   end
 
   def test_my_reduce
-    skip
+    # skip
     func = ->(acc, element) { acc * element }
 
     assert @array.reduce(&func) == @my_array.my_reduce(&func)
