6380d308a816976f4d35dcb440a08aabd8f42e27
Sergei Piadyshev <s.pyadyshev@gmail.com>
2022-12-08 22:26:10 +0400

    Add replace and search solutions

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,36 @@
 module Exercise
   module Arrays
     class << self
+      def findMax(array)
+        $maxNum = 0;
+        array.each_with_index do |num, i|
+          $maxNum < num ? $maxNum = num : $maxNum;
+        end
+        $maxNum
+      end
+
       def replace(array)
-        array
+        $maxNum = findMax(array)
+        array.map { |num| num > 0 ? $maxNum : num }
       end
 
       def search(_array, _query)
-        0
+        low = 0
+        high = _array.length - 1
+        
+          while (low <= high)
+            mid = (low + high) / 2
+            midVal = _array[mid]
+
+            if (midVal < _query)
+                low = mid + 1
+            elsif (midVal > _query)
+                high = mid - 1
+            else
+                return mid
+            end
+          end
+          -1
       end
     end
   end
