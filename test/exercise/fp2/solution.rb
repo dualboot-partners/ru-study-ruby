e8b5f9484e8b03fa70e559f5151f5e50bd64c9b3
Alexander Abramov <snlyshigoto@gmail.com>
2023-05-09 19:13:56 +0700

    created my_each

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,7 +5,16 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        i = 0
+
+        until i >= self.size
+          yield(self[i])
+          i += 1
+        end
+
+        self
+      end
 
       # Написать свою функцию my_map
       def my_map; end
