55a688b358a11aa1fa1ef0773784d50887381a76
Alexander Abramov <snlyshigoto@gmail.com>
2023-05-11 20:11:00 +0700

    my_map completed

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -17,7 +17,9 @@ module Exercise
       end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map;
+        self.my_reduce([]) { |result, n| result << yield(n) }
+      end
 
       # Написать свою функцию my_compact
       def my_compact;
