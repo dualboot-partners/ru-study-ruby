71eaa7d3370cc25d0212275691011185363bcc34
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-05-13 12:27:55 +0500

    exercise/fp2, add my_each

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,7 +5,11 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for item in self
+          yield(item)
+        end
+      end
 
       # Написать свою функцию my_map
       def my_map; end
