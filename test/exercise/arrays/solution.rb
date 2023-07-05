0a9ace88732d7343a45766bb2fc11ee3781eef74
thurrsense <arthur.khasanov20@gmail.com>
2023-07-05 15:22:39 +0300

    Added: Array's tasks solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,33 @@
 module Exercise
   module Arrays
     class << self
+      def max_finder(array)
+        max = array[1]
+        for a in array
+          max = a if a > max
+        end
+        max
+      end
+
       def replace(array)
+        max = max_finder(array)
+        array.each_with_index do |a, index|
+          array[index] = max if a.positive?
+        end
         array
       end
 
-      def search(_array, _query)
-        0
+      def search(_array, _query, low = 0, high = _array.length - 1)
+        return -1 if low > high
+        mid = (high + low) / 2
+        
+        return mid if _array[mid] == _query
+
+        if _array[mid] > _query
+          search(_array, _query, low, mid - 1)
+        else 
+          search(_array, _query, mid + 1, high)
+        end
       end
     end
   end
