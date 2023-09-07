cf898a4aabfba59b1de918f52190728db158d1ee
Григорий Мартынов <grigorii@MacBook-Pro-GS.local>
2023-09-07 16:02:19 +0300

    Solution for first task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,21 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |e| e.positive? ? 100 : e }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, low = 0, high = array.size - 1)
+        return -1 if low > high
+
+        mid = (low + high) / 2
+
+        if query == array[mid]
+          mid
+        elsif query < array[mid]
+          search(array, query, low, mid - 1)
+        else
+          search(array, query, mid + 1, high)
+        end
       end
     end
   end
