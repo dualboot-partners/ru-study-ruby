1ddc7eb5baa8268733165064a1b6a6f420b20727
Mikhail Zaitsau <mikhailzaitsau.work@gmail.com>
2023-10-05 21:54:00 +0300

    Add code for search method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -7,8 +7,18 @@ module Exercise
         array.map! { |el| el.positive? ? maximum_value : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, numerator = 1.0, denominator = 2.0)
+        return -1 if array.empty? || numerator > array.length**2
+
+        index = ((array.length - 1) * (numerator / denominator)).round
+
+        if query < array[index]
+          search(array, query, (numerator * 2) - 1, denominator * 2)
+        elsif query > array[index]
+          search(array, query, (numerator * 2) + 1, denominator * 2)
+        elsif query == array[index]
+          index
+        end
       end
     end
   end
