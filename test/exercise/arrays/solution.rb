750bd4652b8ad7423ab05b679e415808da6224b2
Mikhail Zaitsau <mikhailzaitsau.work@gmail.com>
2023-10-07 15:27:16 +0300

    Fix stack level too deep issue when query less than lowest number in array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -8,7 +8,7 @@ module Exercise
       end
 
       def search(array, query, numerator = 1.0, denominator = 2.0)
-        return -1 if array.empty? || numerator > array.length**2
+        return -1 if array.empty? || denominator > 2**(array.length.to_s.length * 4)
 
         index = ((array.length - 1) * (numerator / denominator)).round
 
