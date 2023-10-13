21803d299d4f0093b0fd026c72205d7a09adfd5f
Pavel-Devel <fominlzpv@gmail.com>
2023-10-13 13:52:46 +0300

    fix solution.rb in exercise arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,38 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_element = array[0]
+
+        array.each do |element|
+          max_element = element if element > max_element
+        end
+
+        array.map do |element|
+          if element >= 0
+            max_element
+          else
+            element
+          end
+        end
       end
 
       def search(_array, _query)
-        0
+        low = 0
+        high = _array.length - 1
+
+        while low <= high
+          mid = (high + low) / 2
+
+          if _array[mid] == _query
+            mid
+          elsif _array[mid] < _query
+            low = mid + 1
+          else
+            high = mid - 1
+          end
+        end
+
+        -1
       end
     end
   end
