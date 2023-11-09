78b2d47b07c197e3db43a79e80f09186f8c31d60
Mikhail Zaitsau <mikhailzaitsau.work@gmail.com>
2023-11-09 12:13:55 +0300

    Improve code readability

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,8 +5,9 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        array.lazy.select { |film| film['rating_kinopoisk'].to_f.positive? }.select { |film| film['country'].to_s.include?(',') }
-             .map { |film| film['rating_kinopoisk'].to_f }.reduce([0, 0]) { |acc, rating| [acc[0] + rating, acc[1] + 1] }.reduce(:/)
+        selected_films = array.lazy.select { |film| film['rating_kinopoisk'].to_f.positive? && film['country'].to_s.split(',').count > 1 }
+                              .map { |film| film['rating_kinopoisk'].to_f }
+        selected_films.reduce(:+) / selected_films.count
       end
 
       def chars_count(films, threshold)
