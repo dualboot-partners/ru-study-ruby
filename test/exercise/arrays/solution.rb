e021e578b951beafe95a8c0e48869f9ded59872b
AndreyChim <chimitcyrenov@mail.ru>
2024-02-12 15:47:11 +0800

    Add def to max & find

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,28 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array[0]
+        array.each do  |i|
+          max = i if i >= max
+        end
+        array.map! { |x| x.positive? ? max : x }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        right = array.length - 1
+        left = 0
+
+        while left <= right
+          mid = (left + right) / 2
+          if array[mid] == query
+            return mid
+          elsif array[mid] > query
+            right = mid - 1
+          else
+            left = mid + 1
+          end
+        end
+        -1
       end
     end
   end
