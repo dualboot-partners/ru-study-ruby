2127ed5f5206d2cf6901feb9eb605fff8833359a
AndreyChim <chimitcyrenov@mail.ru>
2024-03-01 21:32:27 +0800

    Add def rating & chars_count

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,23 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        array = CSV.readlines('./test/fixtures/films.csv', headers: true).to_a
+        rating_kinopoisk = []
+        array.map do |row|
+          rating_kinopoisk.push row[1].to_f if row[6].to_s.split(',').count > 1 && row[1].to_i.positive?
+        end
+        rating_kinopoisk.reduce(:+) / rating_kinopoisk.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(_films, threshold)
+        films = CSV.readlines('./test/fixtures/films.csv', headers: true).to_a
+        name = []
+        films.map do |row|
+          name.push row[0] if row[1].to_f >= threshold && row[0].count('и').positive?
+        end
+
+        sum = name.map { |film| film.count('и') }
+        sum.reduce(:+)
       end
     end
   end
