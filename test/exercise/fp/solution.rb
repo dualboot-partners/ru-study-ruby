52afe4f7c3fe6521c04b4fde6177704724327e8f
AndreyChim <chimitcyrenov@mail.ru>
2024-03-05 22:31:16 +0800

    Fix chars_count

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,21 +4,17 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        array = CSV.readlines('./test/fixtures/films.csv', headers: true)
-
+      def rating(array)
         rating_kinopoisk = (array.map do |film|
                               film['rating_kinopoisk'].to_f if film['country'].to_s.split(',').count > 1 && film['rating_kinopoisk'].to_i.positive?
                             end).compact
         rating_kinopoisk.reduce(:+) / rating_kinopoisk.size
       end
 
-      def chars_count(_films, threshold)
-        films = CSV.readlines('./test/fixtures/films.csv', headers: true)
-        search_letters = films.map do |film|
-          film['name'] if film['name'].count('и').positive? && film['rating_kinopoisk'].to_f >= threshold
-        end.compact
-        search_letters.map { |film| film.count('и') }.reduce(:+)
+      def chars_count(array, threshold)
+        (array.map do |film|
+           film['name'].count('и') if film['name'].count('и').positive? && film['rating_kinopoisk'].to_f >= threshold
+         end.compact).reduce(:+)
       end
     end
   end
