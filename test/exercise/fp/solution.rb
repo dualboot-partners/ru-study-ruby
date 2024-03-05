879066264b59edb1381137547382af0a95f53eee
AndreyChim <chimitcyrenov@mail.ru>
2024-03-05 13:33:59 +0800

    Fix rating && chars_count

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,23 +5,20 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        array = CSV.readlines('./test/fixtures/films.csv', headers: true).to_a
-        rating_kinopoisk = []
-        array.map do |row|
-          rating_kinopoisk.push row[1].to_f if row[6].to_s.split(',').count > 1 && row[1].to_i.positive?
-        end
+        array = CSV.readlines('./test/fixtures/films.csv', headers: true)
+
+        rating_kinopoisk = (array.map do |film|
+                              film['rating_kinopoisk'].to_f if film['country'].to_s.split(',').count > 1 && film['rating_kinopoisk'].to_i.positive?
+                            end).compact
         rating_kinopoisk.reduce(:+) / rating_kinopoisk.size
       end
 
       def chars_count(_films, threshold)
-        films = CSV.readlines('./test/fixtures/films.csv', headers: true).to_a
-        name = []
-        films.map do |row|
-          name.push row[0] if row[1].to_f >= threshold && row[0].count('и').positive?
-        end
-
-        sum = name.map { |film| film.count('и') }
-        sum.reduce(:+)
+        films = CSV.readlines('./test/fixtures/films.csv', headers: true)
+        search_letters = films.map do |film|
+          film['name'] if film['name'].count('и').positive? && film['rating_kinopoisk'].to_f >= threshold
+        end.compact
+        search_letters.map { |film| film.count('и') }.reduce(:+)
       end
     end
   end
