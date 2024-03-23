16f655ddfb1ac2d5095acf0e735eed0cb8d15178
Andrei Akentev <akentev2010@gmail.com>
2024-03-23 20:08:48 +0500

    Add implementation for array replace method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,9 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_value = array.max
+        
+        array.map{ |value| value > 0 && value < max_value ? max_value : value }
       end
 
       def search(_array, _query)
