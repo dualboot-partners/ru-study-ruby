7c86eeeb32ffb72ff3f2f591d1c8c5875d1b2960
Andrei Akentev <akentev2010@gmail.com>
2024-03-24 08:51:08 +0500

    Add implementation for array search method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -8,7 +8,29 @@ module Exercise
       end
 
       def search(_array, _query)
-        0
+        default_return_value = -1
+
+        if _array.empty?
+          return default_return_value
+        end
+
+        start_index = 0
+        end_index = _array.length - 1
+
+        while start_index <= end_index
+          middle_index = start_index + (end_index - start_index) / 2
+          current_value = _array[middle_index]   
+
+          if current_value == _query
+            return middle_index
+          elsif current_value < _query
+            start_index = middle_index + 1
+          else
+            end_index = middle_index - 1
+          end
+        end
+
+        default_return_value
       end
     end
   end
